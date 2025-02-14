f462a2031b5229a19ad649369cdf30c879a9bb97
Solomon Hykes <solomon@dagger.io>
2025-02-14 00:31:03 -0800

    toy-programmer: a very, very simple programmer agent
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "dagger/toy-programmer/internal/dagger"
+
+type ToyProgrammer struct{}
+
+// Write a Go program
+func (m *ToyProgrammer) GoProgram(assignment string) *dagger.Container {
+	// Create a new workspace, using third-party module
+	before := dag.ToyWorkspace()
+	// Run the agent loop in the workspace
+	after := dag.Llm().
+		WithToyWorkspace(before).
+		WithPromptVar("assignment", assignment).
+		WithPromptFile(dag.CurrentModule().Source().File("prompt.txt")).
+		ToyWorkspace()
+	// Return the modified workspace's container
+	return after.Container()
+}
