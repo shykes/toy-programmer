f3cb4d35c6a7d4cd2236145026e12e4b6a3fbd08
Solomon Hykes <solomon@dagger.io>
2025-02-18 14:36:07 -0800

    toy-programmer: call Loop()
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
-import "dagger/toy-programmer/internal/dagger"
+import (
+	"dagger/toy-programmer/internal/dagger"
+	"os"
+)
 
 type ToyProgrammer struct{}
 
 // Write a Go program
 func (m *ToyProgrammer) GoProgram(assignment string) *dagger.Container {
+	// Define env var
+	envBytes := []byte("OPENAI_KEY=foobar\nOPENAI_MODEL=blabla")
+	// Write to .env
+	err := os.WriteFile(".env", envBytes, 0644)
+	if err != nil {
+									panic(err)
+	}
 	// Create a new workspace, using third-party module
 	before := dag.ToyWorkspace()
 	// Run the agent loop in the workspace
@@ -13,6 +23,7 @@ func (m *ToyProgrammer) GoProgram(assignment string) *dagger.Container {
 		WithToyWorkspace(before).
 		WithPromptVar("assignment", assignment).
 		WithPromptFile(dag.CurrentModule().Source().File("prompt.txt")).
+		Loop().
 		ToyWorkspace()
 	// Return the modified workspace's container
 	return after.Container()
