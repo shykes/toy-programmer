8b94a024e5e615835384f1148cf6d0066a4d55bc
Solomon Hykes <solomon@dagger.io>
2025-03-03 16:53:11 -0800

    Optional QA, still 70 lines total :)
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- main.go
+++ main.go
@@ -7,22 +7,22 @@ import (
 type ToyProgrammer struct{}
 
 // Write a Go program
-func (m *ToyProgrammer) GoProgram(assignment string) *dagger.Container {
-	// Create a new workspace, using third-party module
-	before := dag.ToyWorkspace()
-	// Run the agent loop in the workspace
-	after := dag.Llm().
-		WithToyWorkspace(before).
-		WithPromptVar("assignment", assignment).
-		WithPrompt(`
-You are an expert go programmer. You have access to a workspace.
-Use the read, write, build tools to complete the following assignment.
-Do not try to access the container directly.
-Don't stop until your code builds.
-
-Assignment: $assignment
-`).
-		ToyWorkspace()
-	// Return the modified workspace's container
-	return after.Container()
+func (m *ToyProgrammer) GoProgram(assignment string, qa bool) *dagger.Container {
+	result := dag.Llm().
+		WithToyWorkspace(dag.ToyWorkspace().Write("assignment.txt", assignment)).
+		WithPrompt("You are an expert go programmer. You have access to a workspace").
+		WithPrompt("Complete the assignment writte at assignment.txt").
+		WithPrompt("Don't stop until the code builds").
+		ToyWorkspace().
+		Container()
+	if qa {
+		return dag.Llm().
+			WithContainer(result).
+			WithPrompt("You are an expert QA engineer. You have access to a container").
+			WithPrompt("There is a go program in the current directory. Build and run it. Understand what it does. Write your findings in QA.md").
+			WithPrompt("Include a table of each command you ran, and the result").
+			WithPrompt("Be careful not to wipe the state of the container with a new image. Focus on withExec, file, directory").
+			Container()
+	}
+	return result
 }
