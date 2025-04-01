c47ae9c42263217c9557695b472c0b8692501a6b
Jason McCallister <jason@mccallister.dev>
2025-04-01 17:21:25 +0100

    update dagger
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

--- main.go
+++ main.go
@@ -14,21 +14,36 @@ func (m *ToyProgrammer) GoProgram(
 	// Run LLM-powered QA on the result
 	// +optional
 	qa bool) *dagger.Container {
+	ws := dag.ToyWorkspace().Write("assignment.txt", assignment)
+
+	env := dag.Env().
+		WithToyWorkspaceInput("workspace", ws, "The workspace for the assignment").
+		WithToyWorkspaceOutput("workspace", "The workspace with the results of the assignment")
+
 	result := dag.LLM().
-		WithToyWorkspace(dag.ToyWorkspace().Write("assignment.txt", assignment)).
+		WithEnv(env).
 		WithPrompt("You are an expert go programmer. You have access to a workspace").
 		WithPrompt("Complete the assignment written at assignment.txt").
 		WithPrompt("Don't stop until the code builds").
-		ToyWorkspace().
+		Env().
+		Output("workspace").
+		AsToyWorkspace().
 		Container()
 	if qa {
+		qaEnv := dag.Env().
+			WithContainerInput("results", result, "The container with the results of the assignment").
+			WithContainerOutput("results", "The container with the QA results")
+
 		return dag.LLM().
-			WithContainer(result).
+			WithEnv(qaEnv).
 			WithPrompt("You are an expert QA engineer. You have access to a container").
 			WithPrompt("There is a go program in the current directory. Build and run it. Understand what it does. Write your findings in QA.md").
 			WithPrompt("Include a table of each command you ran, and the result").
 			WithPrompt("Be careful not to wipe the state of the container with a new image. Focus on withExec, file, directory").
-			Container()
+			Env().
+			Output("results").
+			AsContainer()
 	}
+
 	return result
 }
